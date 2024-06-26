27dadb33ee8e2c2e4950a6c5aa444aa6262ed551
mikedarcy <mikedarcy@users.noreply.github.com>
2024-06-26 00:30:18 -0700

    Initial commit.

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# deriva-site-template
-Basic Jekyll website template for DERIVA deployments. 
-
-You can generate the website content by running `jekyll build` from the `www` subdirectory.
-
+# LLM4ISI Project Repository
+This repository contains static website content and DERIVA configuration files for the LLM4ISI pilot project. 
+It is also a place to track project issues.
--- config/bootstrap-site.sh
+++ config/bootstrap-site.sh
@@ -31,6 +31,7 @@ echo "NavbarBrandText = \"${label:=${host}}"' Data Browser'\"
 
 deriva-catalog-cli --host ${host} create --id "${catalog}" \
 --owner 'https://auth.globus.org/3938e0d0-ed35-11e5-8641-22000ab4b42b' \
+'https://auth.globus.org/324ff4ca-332c-11ef-ad7c-c5e0bfb8d54a' \
 --auto-configure \
 --configure-args includeWWWSchema='False' \
 publicSchemaDisplayName='User Info' \
@@ -38,3 +39,31 @@ headTitle="${label:=${host}}" \
 navbarBrandText="${label:=${host}}"' Data Browser'
 
 deriva-acl-config --host ${host} --config ./self_serve_policy.json "${catalog}"
+
+deriva-hatrac-cli --host ${host} setacl /hatrac/ owner \
+'https://auth.globus.org/3938e0d0-ed35-11e5-8641-22000ab4b42b' \
+'https://auth.globus.org/324ff4ca-332c-11ef-ad7c-c5e0bfb8d54a'
+
+deriva-hatrac-cli --host ${host} setacl /hatrac/ create \
+'https://auth.globus.org/3938e0d0-ed35-11e5-8641-22000ab4b42b' \
+'https://auth.globus.org/324ff4ca-332c-11ef-ad7c-c5e0bfb8d54a' \
+'https://auth.globus.org/d2f1eb28-3330-11ef-ad7c-c5e0bfb8d54a'
+
+deriva-hatrac-cli --host ${host} setacl /hatrac/ subtree-owner \
+'https://auth.globus.org/3938e0d0-ed35-11e5-8641-22000ab4b42b' \
+'https://auth.globus.org/324ff4ca-332c-11ef-ad7c-c5e0bfb8d54a'
+
+deriva-hatrac-cli --host ${host} setacl /hatrac/ subtree-create \
+'https://auth.globus.org/3938e0d0-ed35-11e5-8641-22000ab4b42b' \
+'https://auth.globus.org/324ff4ca-332c-11ef-ad7c-c5e0bfb8d54a' \
+'https://auth.globus.org/d2f1eb28-3330-11ef-ad7c-c5e0bfb8d54a' \
+'https://auth.globus.org/4906b470-3330-11ef-ad7c-c5e0bfb8d54a'
+
+deriva-hatrac-cli --host ${host} setacl /hatrac/ subtree-update \
+'https://auth.globus.org/d2f1eb28-3330-11ef-ad7c-c5e0bfb8d54a' \
+'https://auth.globus.org/4906b470-3330-11ef-ad7c-c5e0bfb8d54a'
+
+deriva-hatrac-cli --host ${host} setacl /hatrac/ subtree-read \
+'https://auth.globus.org/d2f1eb28-3330-11ef-ad7c-c5e0bfb8d54a' \
+'https://auth.globus.org/4906b470-3330-11ef-ad7c-c5e0bfb8d54a' \
+'https://auth.globus.org/c2def0ae-332c-11ef-8ec4-1df34dfa5fb6'
